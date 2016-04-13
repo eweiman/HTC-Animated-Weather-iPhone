7a1c4dd398741779fb2622615a3b81bea620473d
Erik Weiman <erik.weiman@isilon.com>
2016-04-12 20:59:39 -0500

    Initial commit to GitHub

---
+++ HTC Weather OSX/Private/translation.js
@@ -0,0 +1,72 @@
+function ForecastDayNames(day){
+
+switch (day)
+  {
+//Use these to translate if necessary.  Change the second values, leave the first alone.
+    case "Mon": { return "Mon" }
+    case "Tue": { return "Tue" }
+    case "Wed": { return "Wed" }
+    case "Thu": { return "Thu" }
+    case "Fri": { return "Fri" }
+    case "Sat": { return "Sat" }
+    case "Sun": { return "Sun" }
+    case "Today": { return "Today" }
+    case "Tonight": { return "Tonight" }
+  }
+
+}
+
+var Italian =
+[
+"Tornado",
+"Tempesta Tropicale",
+"Uragano",
+"Temporali Forti",
+"Temporali",
+"Pioggia mista a Neve",
+"Nevischio",
+"Nevischio",
+"Pioggia Gelata",
+"Pioggerella",
+"Pioggia Ghiacciata",
+"Pioggia",
+"Pioggia",
+"Neve a Raffiche",
+"Neve Leggera",
+"Tempesta di Neve",
+"Neve",
+"Grandine",
+"Nevischio",
+"Irregolare",
+"Nebbia",
+"Foschia",
+"Fumoso",
+"Raffiche di Vento",
+"Ventoso",
+"Freddo",
+"Nuvoloso",
+"Molto Nuvoloso",
+"Molto Nuvoloso",
+"Parzialmente Nuvoloso",
+"Parzialmente Nuvoloso",
+"Sereno",
+"Sereno",
+"Bel Tempo",
+"Bel Tempo",
+"Pioggia e Grandine",
+"Caldo",
+"Temporali Isolati",
+"Temporali Sparsi",
+"Temporali Sparsi",
+"Rovesci Sparsi",
+"Neve Forte",
+"Nevicate Sparse",
+"Neve Forte",
+"Parzialmente Nuvoloso",
+"Rovesci Temporaleschi",
+"Rovesci di Neve",
+"Temporali isolati",
+"Non Disponibile"
+]
+
+var English = "English" //Leave Alone
